ba83cddceaecb1795eb0bf45821bf2b4b3662068
Hüseyin Ulaş Karabıyık <45794836+HuseyinUlasKarabiyik@users.noreply.github.com>
2026-02-10 21:04:14 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+## Kaynaklar ve Teşekkür (Credits)
+
+Bu proje geliştirilirken aşağıdaki açık kaynak verilerden ve kütüphanelerden yararlanılmıştır:
+
+* **Türkiye İl Sınırları Verisi:** [tr-geojson](https://github.com/cihadturhan/tr-geojson) deposu için **Cihad Turhan**'a teşekkürler.
+    * *Kullanılan Veri Seti:* `tr-cities-utf8.json`
+* **Harita Verileri:** [OpenStreetMap](https://www.openstreetmap.org/copyright) katkı verenleri (ODbL Lisansı kapsamında).
+* **Harita Motoru:** [Leaflet.js](https://leafletjs.com/)
+* **Altlık Harita (Tiles):** Esri World Imagery
+
+---
+*Bu proje eğitim amaçlı geliştirilmiştir ve ticari bir amacı yoktur.*
---
+++ sorular.js
@@ -0,0 +1,519 @@
+var Soru_Havuzu =
+    [
+        {
+            "id": 1,
+            "soru_metni": "Dünyaca ünlü Falez oluşumları ve Düden Şelalesi bu ilimizdedir.",
+            "cevap_ili": "Antalya",
+            "asama": "zor"
+        },
+        {
+            "id": 2,
+            "soru_metni": "Türkiye'nin başkenti ve Anıtkabir'in bulunduğu ilimiz hangisidir?",
+            "cevap_ili": "Ankara",
+            "asama": "zor"
+        },
+        {
+            "id": 3,
+            "soru_metni": "Nemrut Dağı heykellerine ev sahipliği yapan ilimiz?",
+            "cevap_ili": "Adıyaman",
+            "asama": "zor"
+        },
+        {
+            "id": 4,
+            "soru_metni": "Türkiye'nin en yüksek dağı olan Ağrı Dağı'nın büyük bölümü buradadır.",
+            "cevap_ili": "Ağrı",
+            "asama": "zor"
+        },
+        {
+            "id": 5,
+            "soru_metni": "Şehzadeler şehri olarak bilinen ve elmalarıyla ünlü ilimiz?",
+            "cevap_ili": "Amasya",
+            "asama": "zor"
+        },
+        {
+            "id": 6,
+            "soru_metni": "Adana İli Haritada nerededir?",
+            "cevap_ili": "Adana",
+            "asama": "kolay"
+        },
+        {
+            "id": 7,
+            "soru_metni": "Adıyaman İli Haritada nerededir?",
+            "cevap_ili": "Adıyaman",
+            "asama": "kolay"
+        },
+        {
+            "id": 8,
+            "soru_metni": "Afyonkarahisar İli Haritada nerededir?",
+            "cevap_ili": "Afyon",
+            "asama": "kolay"
+        },
+        {
+            "id": 9,
+            "soru_metni": "Ağrı İli Haritada nerededir?",
+            "cevap_ili": "Ağrı",
+            "asama": "kolay"
+        },
+        {
+            "id": 10,
+            "soru_metni": "Amasya İli Haritada nerededir?",
+            "cevap_ili": "Amasya",
+            "asama": "kolay"
+        },
+        {
+            "id": 11,
+            "soru_metni": "Ankara İli Haritada nerededir?",
+            "cevap_ili": "Ankara",
+            "asama": "kolay"
+        },
+        {
+            "id": 12,
+            "soru_metni": "Antalya İli Haritada nerededir?",
+            "cevap_ili": "Antalya",
+            "asama": "kolay"
+        },
+        {
+            "id": 13,
+            "soru_metni": "Artvin İli Haritada nerededir?",
+            "cevap_ili": "Artvin",
+            "asama": "kolay"
+        },
+        {
+            "id": 14,
+            "soru_metni": "Aydın İli Haritada nerededir?",
+            "cevap_ili": "Aydın",
+            "asama": "kolay"
+        },
+        {
+            "id": 15,
+            "soru_metni": "Balıkesir İli Haritada nerededir?",
+            "cevap_ili": "Balıkesir",
+            "asama": "kolay"
+        },
+        {
+            "id": 16,
+            "soru_metni": "Bilecik İli Haritada nerededir?",
+            "cevap_ili": "Bilecik",
+            "asama": "kolay"
+        },
+        {
+            "id": 17,
+            "soru_metni": "Bingöl İli Haritada nerededir?",
+            "cevap_ili": "Bingöl",
+            "asama": "kolay"
+        },
+        {
+            "id": 18,
+            "soru_metni": "Bitlis İli Haritada nerededir?",
+            "cevap_ili": "Bitlis",
+            "asama": "kolay"
+        },
+        {
+            "id": 19,
+            "soru_metni": "Bolu İli Haritada nerededir?",
+            "cevap_ili": "Bolu",
+            "asama": "kolay"
+        },
+        {
+            "id": 20,
+            "soru_metni": "Burdur İli Haritada nerededir?",
+            "cevap_ili": "Burdur",
+            "asama": "kolay"
+        },
+        {
+            "id": 21,
+            "soru_metni": "Bursa İli Haritada nerededir?",
+            "cevap_ili": "Bursa",
+            "asama": "kolay"
+        },
+        {
+            "id": 22,
+            "soru_metni": "Çanakkale İli Haritada nerededir?",
+            "cevap_ili": "Çanakkale",
+            "asama": "kolay"
+        },
+        {
+            "id": 23,
+            "soru_metni": "Çankırı İli Haritada nerededir?",
+            "cevap_ili": "Çankırı",
+            "asama": "kolay"
+        },
+        {
+            "id": 24,
+            "soru_metni": "Çorum İli Haritada nerededir?",
+            "cevap_ili": "Çorum",
+            "asama": "kolay"
+        },
+        {
+            "id": 25,
+            "soru_metni": "Denizli İli Haritada nerededir?",
+            "cevap_ili": "Denizli",
+            "asama": "kolay"
+        },
+        {
+            "id": 26,
+            "soru_metni": "Diyarbakır İli Haritada nerededir?",
+            "cevap_ili": "Diyarbakır",
+            "asama": "kolay"
+        },
+        {
+            "id": 27,
+            "soru_metni": "Edirne İli Haritada nerededir?",
+            "cevap_ili": "Edirne",
+            "asama": "kolay"
+        },
+        {
+            "id": 28,
+            "soru_metni": "Elazığ İli Haritada nerededir?",
+            "cevap_ili": "Elazığ",
+            "asama": "kolay"
+        },
+        {
+            "id": 29,
+            "soru_metni": "Erzincan İli Haritada nerededir?",
+            "cevap_ili": "Erzincan",
+            "asama": "kolay"
+        },
+        {
+            "id": 30,
+            "soru_metni": "Erzurum İli Haritada nerededir?",
+            "cevap_ili": "Erzurum",
+            "asama": "kolay"
+        },
+        {
+            "id": 31,
+            "soru_metni": "Eskişehir İli Haritada nerededir?",
+            "cevap_ili": "Eskişehir",
+            "asama": "kolay"
+        },
+        {
+            "id": 32,
+            "soru_metni": "Gaziantep İli Haritada nerededir?",
+            "cevap_ili": "Gaziantep",
+            "asama": "kolay"
+        },
+        {
+            "id": 33,
+            "soru_metni": "Giresun İli Haritada nerededir?",
+            "cevap_ili": "Giresun",
+            "asama": "kolay"
+        },
+        {
+            "id": 34,
+            "soru_metni": "Gümüşhane İli Haritada nerededir?",
+            "cevap_ili": "Gümüşhane",
+            "asama": "kolay"
+        },
+        {
+            "id": 35,
+            "soru_metni": "Hakkari İli Haritada nerededir?",
+            "cevap_ili": "Hakkari",
+            "asama": "kolay"
+        },
+        {
+            "id": 36,
+            "soru_metni": "Hatay İli Haritada nerededir?",
+            "cevap_ili": "Hatay",
+            "asama": "kolay"
+        },
+        {
+            "id": 37,
+            "soru_metni": "Isparta İli Haritada nerededir?",
+            "cevap_ili": "Isparta",
+            "asama": "kolay"
+        },
+        {
+            "id": 38,
+            "soru_metni": "Mersin İli Haritada nerededir?",
+            "cevap_ili": "Mersin",
+            "asama": "kolay"
+        },
+        {
+            "id": 39,
+            "soru_metni": "İstanbul İli Haritada nerededir?",
+            "cevap_ili": "İstanbul",
+            "asama": "kolay"
+        },
+        {
+            "id": 40,
+            "soru_metni": "İzmir İli Haritada nerededir?",
+            "cevap_ili": "İzmir",
+            "asama": "kolay"
+        },
+        {
+            "id": 41,
+            "soru_metni": "Kars İli Haritada nerededir?",
+            "cevap_ili": "Kars",
+            "asama": "kolay"
+        },
+        {
+            "id": 42,
+            "soru_metni": "Kastamonu İli Haritada nerededir?",
+            "cevap_ili": "Kastamonu",
+            "asama": "kolay"
+        },
+        {
+            "id": 43,
+            "soru_metni": "Kayseri İli Haritada nerededir?",
+            "cevap_ili": "Kayseri",
+            "asama": "kolay"
+        },
+        {
+            "id": 44,
+            "soru_metni": "Kırklareli İli Haritada nerededir?",
+            "cevap_ili": "Kırklareli",
+            "asama": "kolay"
+        },
+        {
+            "id": 45,
+            "soru_metni": "Kırşehir İli Haritada nerededir?",
+            "cevap_ili": "Kırşehir",
+            "asama": "kolay"
+        },
+        {
+            "id": 46,
+            "soru_metni": "Kocaeli İli Haritada nerededir?",
+            "cevap_ili": "Kocaeli",
+            "asama": "kolay"
+        },
+        {
+            "id": 47,
+            "soru_metni": "Konya İli Haritada nerededir?",
+            "cevap_ili": "Konya",
+            "asama": "kolay"
+        },
+        {
+            "id": 48,
+            "soru_metni": "Kütahya İli Haritada nerededir?",
+            "cevap_ili": "Kütahya",
+            "asama": "kolay"
+        },
+        {
+            "id": 49,
+            "soru_metni": "Malatya İli Haritada nerededir?",
+            "cevap_ili": "Malatya",
+            "asama": "kolay"
+        },
+        {
+            "id": 50,
+            "soru_metni": "Manisa İli Haritada nerededir?",
+            "cevap_ili": "Manisa",
+            "asama": "kolay"
+        },
+        {
+            "id": 51,
+            "soru_metni": "Kahramanmaraş İli Haritada nerededir?",
+            "cevap_ili": "Kahramanmaraş",
+            "asama": "kolay"
+        },
+        {
+            "id": 52,
+            "soru_metni": "Mardin İli Haritada nerededir?",
+            "cevap_ili": "Mardin",
+            "asama": "kolay"
+        },
+        {
+            "id": 53,
+            "soru_metni": "Muğla İli Haritada nerededir?",
+            "cevap_ili": "Muğla",
+            "asama": "kolay"
+        },
+        {
+            "id": 54,
+            "soru_metni": "Muş İli Haritada nerededir?",
+            "cevap_ili": "Muş",
+            "asama": "kolay"
+        },
+        {
+            "id": 55,
+            "soru_metni": "Nevşehir İli Haritada nerededir?",
+            "cevap_ili": "Nevşehir",
+            "asama": "kolay"
+        },
+        {
+            "id": 56,
+            "soru_metni": "Niğde İli Haritada nerededir?",
+            "cevap_ili": "Niğde",
+            "asama": "kolay"
+        },
+        {
+            "id": 57,
+            "soru_metni": "Ordu İli Haritada nerededir?",
+            "cevap_ili": "Ordu",
+            "asama": "kolay"
+        },
+        {
+            "id": 58,
+            "soru_metni": "Rize İli Haritada nerededir?",
+            "cevap_ili": "Rize",
+            "asama": "kolay"
+        },
+        {
+            "id": 59,
+            "soru_metni": "Sakarya İli Haritada nerededir?",
+            "cevap_ili": "Sakarya",
+            "asama": "kolay"
+        },
+        {
+            "id": 60,
+            "soru_metni": "Samsun İli Haritada nerededir?",
+            "cevap_ili": "Samsun",
+            "asama": "kolay"
+        },
+        {
+            "id": 61,
+            "soru_metni": "Siirt İli Haritada nerededir?",
+            "cevap_ili": "Siirt",
+            "asama": "kolay"
+        },
+        {
+            "id": 62,
+            "soru_metni": "Sinop İli Haritada nerededir?",
+            "cevap_ili": "Sinop",
+            "asama": "kolay"
+        },
+        {
+            "id": 63,
+            "soru_metni": "Sivas İli Haritada nerededir?",
+            "cevap_ili": "Sivas",
+            "asama": "kolay"
+        },
+        {
+            "id": 64,
+            "soru_metni": "Tekirdağ İli Haritada nerededir?",
+            "cevap_ili": "Tekirdağ",
+            "asama": "kolay"
+        },
+        {
+            "id": 65,
+            "soru_metni": "Tokat İli Haritada nerededir?",
+            "cevap_ili": "Tokat",
+            "asama": "kolay"
+        },
+        {
+            "id": 66,
+            "soru_metni": "Trabzon İli Haritada nerededir?",
+            "cevap_ili": "Trabzon",
+            "asama": "kolay"
+        },
+        {
+            "id": 67,
+            "soru_metni": "Tunceli İli Haritada nerededir?",
+            "cevap_ili": "Tunceli",
+            "asama": "kolay"
+        },
+        {
+            "id": 68,
+            "soru_metni": "Şanlıurfa İli Haritada nerededir?",
+            "cevap_ili": "Şanlıurfa",
+            "asama": "kolay"
+        },
+        {
+            "id": 69,
+            "soru_metni": "Uşak İli Haritada nerededir?",
+            "cevap_ili": "Uşak",
+            "asama": "kolay"
+        },
+        {
+            "id": 70,
+            "soru_metni": "Van İli Haritada nerededir?",
+            "cevap_ili": "Van",
+            "asama": "kolay"
+        },
+        {
+            "id": 71,
+            "soru_metni": "Yozgat İli Haritada nerededir?",
+            "cevap_ili": "Yozgat",
+            "asama": "kolay"
+        },
+        {
+            "id": 72,
+            "soru_metni": "Zonguldak İli Haritada nerededir?",
+            "cevap_ili": "Zonguldak",
+            "asama": "kolay"
+        },
+        {
+            "id": 73,
+            "soru_metni": "Aksaray İli Haritada nerededir?",
+            "cevap_ili": "Aksaray",
+            "asama": "kolay"
+        },
+        {
+            "id": 74,
+            "soru_metni": "Bayburt İli Haritada nerededir?",
+            "cevap_ili": "Bayburt",
+            "asama": "kolay"
+        },
+        {
+            "id": 75,
+            "soru_metni": "Karaman İli Haritada nerededir?",
+            "cevap_ili": "Karaman",
+            "asama": "kolay"
+        },
+        {
+            "id": 76,
+            "soru_metni": "Kırıkkale İli Haritada nerededir?",
+            "cevap_ili": "Kırıkkale",
+            "asama": "kolay"
+        },
+        {
+            "id": 77,
+            "soru_metni": "Batman İli Haritada nerededir?",
+            "cevap_ili": "Batman",
+            "asama": "kolay"
+        },
+        {
+            "id": 78,
+            "soru_metni": "Şırnak İli Haritada nerededir?",
+            "cevap_ili": "Şırnak",
+            "asama": "kolay"
+        },
+        {
+            "id": 79,
+            "soru_metni": "Bartın İli Haritada nerededir?",
+            "cevap_ili": "Bartın",
+            "asama": "kolay"
+        },
+        {
+            "id": 80,
+            "soru_metni": "Ardahan İli Haritada nerededir?",
+            "cevap_ili": "Ardahan",
+            "asama": "kolay"
+        },
+        {
+            "id": 81,
+            "soru_metni": "Iğdır İli Haritada nerededir?",
+            "cevap_ili": "Iğdır",
+            "asama": "kolay"
+        },
+        {
+            "id": 82,
+            "soru_metni": "Yalova İli Haritada nerededir?",
+            "cevap_ili": "Yalova",
+            "asama": "kolay"
+        },
+        {
+            "id": 83,
+            "soru_metni": "Karabük İli Haritada nerededir?",
+            "cevap_ili": "Karabük",
+            "asama": "kolay"
+        },
+        {
+            "id": 84,
+            "soru_metni": "Kilis İli Haritada nerededir?",
+            "cevap_ili": "Kilis",
+            "asama": "kolay"
+        },
+        {
+            "id": 85,
+            "soru_metni": "Osmaniye İli Haritada nerededir?",
+            "cevap_ili": "Osmaniye",
+            "asama": "kolay"
+        },
+        {
+            "id": 86,
+            "soru_metni": "Düzce İli Haritada nerededir?",
+            "cevap_ili": "Düzce",
+            "asama": "kolay"
+        }
+    ];
